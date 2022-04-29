829ecaeeab1f32551026bb4e328a24ee6dcfac21
Park donghyeon <ppd0523@bbrick.com>
2022-04-29 16:57:41 +0900

    Notice home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import comubirdsImg from './resources/comubird_home.jpeg';
 
 function App() {
   return (
-    <div style={{margin: '50px'}}>
-      <h1 style={{fontSize: 30}}>Notice</h1>
-      <p style={{fontSize: 20}}>COMU Bird is coming! See you in June</p>
-    </div>
+    <>
+      <div style={{margin: '50px auto', textAlign: "center"}}>
+        <h1 style={{fontSize: 20}}>껄무버드 6월 31일 출시 예정!</h1>
+        <img src={comubirdsImg} alt="home page" width="80%" />
+        <p>emma415g@gmail.com</p>
+      </div>
+    </>
   );
 }
 
